8ef9e7459cd539f6968ed8db9849d3206a92bbc7
sarumathibtech <sarumathimuthusamy@gmail.com>
2016-07-25 06:50:33 -0700

    Create even_num.java

---
+++ even_num.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class even_num
+{
+	public static void main(String args[])
+	{
+			int start,end,i;
+			Scanner ob=new Scanner(System.in);
+			System.out.println("Enter the start and end interval:");
+			start=ob.nextInt();
+			end=ob.nextInt();
+			
+		for(i=start;i<=end;i++)
+		{
+			if(i%2==0)
+				System.out.print(i+" ");
+		}
+			
+	}
+}
