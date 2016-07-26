feedd0ca72ffe9e7af2c02283f626a792f1ab40e
sarumathibtech <sarumathimuthusamy@gmail.com>
2016-07-26 08:46:41 -0700

    Update gcd_of_two_num.java

--- gcd_of_two_num.java
+++ gcd_of_two_num.java
@@ -1,24 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-public class gcd
+public class gcd_of_two_num
 {
 	public static void main(String args[])
 	{
-		int a,b,i;
+		int p,q,i;
 		Scanner ob=new Scanner(System.in);
 		
-		System.out.println("enter the a and b value:");
-		a=ob.nextInt();
-		b=ob.nextInt();
-	int	max=Math.max(a,b);
+		System.out.println("enter the p and q value:");
+		p=ob.nextInt();
+		q=ob.nextInt();
+	int	max=Math.max(p,q);
 		System.out.print(max);
 		for(i=max;i>=1;i--)
 		{
 				
-				if(a%i==0 && b%i==0)
+				if(p%i==0 && q%i==0)
 				{
-					System.out.print("gcd of "+a+" and "+b+" is "+i);
+					System.out.print("gcd of "+p+" and "+q+" is "+i);
 					break;
 				}
 				
